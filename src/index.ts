79d74d626a38be8882113ae2103c87ae308f6d45
kobakakzu0429 <kobakazu10@gmail.com>
2020-11-30 15:37:33 +0900

    add weekend

--- src/index.ts
+++ src/index.ts
@@ -14,9 +14,15 @@ if (!USERNAME || !PASSWORD)
 const today = moment().tz("Asia/Tokyo");
 const month = today.month() + 1;
 const date = today.date();
+const day = today.day();
 
 console.log(today);
-console.log(month, date);
+console.log(month, date, day);
+
+if ([0, 6].includes(day)) {
+  console.log("weekend.");
+  process.exit();
+}
 
 (async () => {
   const moodle = new AutoBodyTempMesument(USERNAME, PASSWORD);
